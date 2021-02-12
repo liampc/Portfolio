d356cc5ffdf726fad27c46a34ddd762d3eb9162c
liampc <kathliampcarlos@gmail.com>
2021-02-12 14:15:51 +0800

    link header

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@
 import './_App.scss';
 import './main.scss';
 
+import Header from './layout/Header';
 
 function App() {
   return (
     <div className="App">
-      Hellow World
+      <Header />
     </div>
   );
 }
---
+++ src/layout/Header.js
@@ -0,0 +1,20 @@
+import react, {Component} from 'react'
+
+
+import './_header.scss';
+
+
+
+class Header extends Component {
+
+    render(){
+
+        return (
+            <header>
+                <a href="#">Kathlia Carlos</a>
+            </header>
+        )
+    }
+}
+
+export default Header
