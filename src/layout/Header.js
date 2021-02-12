d6218f9ebcc6291a4d07daa1b73d919e0b3a8dce
liampc <kathliampcarlos@gmail.com>
2021-02-12 17:12:07 +0800

    add handleClick for toggle

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,3 +1,4 @@
+// import { FALSE } from 'node-sass';
 import react, {Component} from 'react'
 
 
@@ -7,11 +8,35 @@ import './_header.scss';
 
 class Header extends Component {
 
+    constructor(props){
+        super(props)
+            this.state = {
+                isToggleOn : false
+            }
+
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+
+    handleClick(){
+        let header = document.querySelector('.header')
+        
+        this.setState(state => ({
+            isToggleOn: !state.isToggleOn
+        }))
+        
+        this.state.isToggleOn == false ? header.classList.add('open') : 
+        header.classList.remove('open')
+
+    }
+
+
     render(){
 
+
         return (
-            <header className="header container open">
-                <a className="header__toggle">
+            <header className="header container ">
+                <a className="header__toggle" onClick={this.handleClick}>
                     <span></span>
                     <span></span>
                     <span></span>
