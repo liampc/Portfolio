14940f03df489a4bd52eb8fdf59cd3a20e15245b
liampc <kathliampcarlos@gmail.com>
2021-02-13 09:19:17 +0800

    link Home to App

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './_App.scss';
 import './main.scss';
 
 import Header from './layout/header/Header';
+import Home from './layout/home/Home'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <main>
+          <Home />
+      </main>
     </div>
   );
 }
---
+++ src/layout/home/Home.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import './_home.scss'
+
+class Home extends Component {
+
+
+    render(){
+        return (
+            <section>
+                <h1>Kathlia Carlos</h1>
+                <h2>Frontend developer</h2>
+            </section>
+        )
+    }
+}
+
+export default Home
