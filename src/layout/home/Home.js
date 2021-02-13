65e15a6b1e3b0ada5f258af395a4170875f5595d
liampc <kathliampcarlos@gmail.com>
2021-02-13 11:13:50 +0800

    add container div home

--- src/layout/home/Home.js
+++ src/layout/home/Home.js
@@ -7,11 +7,13 @@ class Home extends Component {
     render(){
         return (
             <section className="home">
-                <div className="home__title container">
-                    <h1>Kathlia Carlos</h1>
-                    <p>Front-end developer</p>
+                <div className="home__container container">
+                    <div className="home__title">
+                        <h1>Kathlia Carlos</h1>
+                        <p>Front-end developer</p>
+                    </div>
+                    <div className="home__image"></div>
                 </div>
-                <div className="home__image"></div>
             </section>
         )
     }
