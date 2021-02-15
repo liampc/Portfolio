bb5763ae49230acdea30a5056def8d9b0e896399
liampc <kathliampcarlos@gmail.com>
2021-02-15 10:44:16 +0800

    add grid to about

--- src/layout/about/About.js
+++ src/layout/about/About.js
@@ -10,8 +10,8 @@ class About extends Component {
     render(){
         return (
             <section className="about container-pt">
-                 <Subheader title={'About Me'} />
-                <div className="about__container container">
+                 <Subheader title={'About Me'}/>
+                <div className="about__container container container-pall">
                     <div>
                         <p>This an introduction text. It has a fixed size, and a custom line height, so you can experiment with it. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus vulputate diam eu pretium.</p>
                         <p>This an introduction text. It has a fixed size, and a custom line height, so you can experiment with it. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus vulputate diam eu pretium.</p>
