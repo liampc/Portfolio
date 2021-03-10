67af919e175aede806da1030ef0cdeb3cca87f82
liampc <kathliampcarlos@gmail.com>
2021-03-10 13:41:05 +0800

    add header div

---
+++ src/components/header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './_header.scss';
+
+function Header(){
+
+    return (
+    
+        <header className="header">
+
+        </header>
+    )
+   
+}
+
+export default Header
