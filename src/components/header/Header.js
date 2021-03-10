a3e60865fb0d4239cb28d944d3be232c8a121382
liampc <kathliampcarlos@gmail.com>
2021-03-10 17:00:24 +0800

    add profile pic

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,21 +1,25 @@
 import React from 'react'
 
 import './_header.scss';
+import image from '../../assets/Profile.jpg';
 
 function Header(){
 
     return (
     
         <header className="header">
-            <nav className="header__nav padding-lr">
-                <a href="#">About Me</a>
-                <a href="#">Works</a>
-                <a href="#">Contact</a>
-            </nav>
-            <div className="header__titles">
-                <h1 className="padding-lr">Kathlia Carlos</h1>
-                <p className="padding-lr">Frontend Developer</p>
+            <div>
+                <nav className="header__nav padding-lr">
+                    <a href="#">About Me</a>
+                    <a href="#">Works</a>
+                    <a href="#">Contact</a>
+                </nav>
+                <div className="header__titles">
+                    <h1 className="padding-lr">Kathlia Carlos</h1>
+                    <p className="padding-lr">Frontend Developer</p>
+                </div>
             </div>
+            <img className="header__profile" src={image} alt="profilePic" />
         </header>
     )
    
