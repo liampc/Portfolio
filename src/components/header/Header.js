9463bdd9349bd065434a10a56a2b9770fcd7138a
liampc <kathliampcarlos@gmail.com>
2021-03-10 16:32:39 +0800

    edit header titles

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,12 +7,15 @@ function Header(){
     return (
     
         <header className="header">
-            <nav className="header__nav container">
+            <nav className="header__nav padding-lr">
                 <a href="#">About Me</a>
                 <a href="#">Works</a>
                 <a href="#">Contact</a>
             </nav>
-
+            <div className="header__titles">
+                <h1 className="padding-lr">Kathlia Carlos</h1>
+                <p className="padding-lr">Frontend Developer</p>
+            </div>
         </header>
     )
    
