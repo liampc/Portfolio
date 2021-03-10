bc29738f05ef0c36a92ee9f9e3711397ec87a952
liampc <kathliampcarlos@gmail.com>
2021-03-10 13:55:14 +0800

    show hide header nav

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,6 +7,11 @@ function Header(){
     return (
     
         <header className="header">
+            <nav className="header__nav container">
+                <a href="#">About Me</a>
+                <a href="#">Works</a>
+                <a href="#">Contact</a>
+            </nav>
 
         </header>
     )
