aec5e1faea2cec31d39700a1fdc2352b01923a42
liampc <kathliampcarlos@gmail.com>
2021-03-11 16:47:19 +0800

    add temp icons section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.scss';
 import Header from './components/header/Header'
 import About from './components/about/About'
+import Icons from './components/icons/Icons'
 
 function App() {
   return (
     <div className="App">
         <Header />
         <About />
+        <Icons />
     </div>
   );
 }
---
+++ src/components/icons/Icons.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './_icons.scss'
+
+import html from '../../assets/html.png'
+import css from '../../assets/css.png'
+import js from '../../assets/js.png'
+import react from '../../assets/react.png'
+
+function Icons(){
+
+    return(
+        <section className="icons padding-all">
+            <img className="icons__image" src={html} alt="html" />
+            <img className="icons__image" src={css} alt="css" />
+            <img className="icons__image" src={js} alt="js" />
+            <img className="icons__image" src={react} alt="react" />
+        </section>
+    )
+}
+
+export default Icons
