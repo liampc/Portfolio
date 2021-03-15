65e2a7d4bfc359e035d155a6891681f59a01f6f2
liampc <kathliampcarlos@gmail.com>
2021-03-15 15:07:28 +0800

    edit footer icons

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,17 +4,26 @@ import './_footer.scss'
 function Footer(){
     return(
         <footer className="footer">
-            <div className="container__pd-all">
+            <div className="footer__container container__pd-all">
                 <h2 id="contact" className="subheader">Contact</h2>
                 <div className="footer__quote">
                     <p>"Chance encounters are what keep us going"</p>
                     <p>-Kafka on the Shore, Haruki Murakami</p>
                 </div>
-                <p>kathliampcarlos@gmail.com</p>
-                <div className="footer__icons">
-                    <img src="https://img.icons8.com/ios-filled/50/000000/github.png"/>
-                    <img src="https://img.icons8.com/fluent/48/000000/gmail--v2.png"/>
-                    <img src="https://img.icons8.com/color/48/000000/linkedin.png"/>
+                <div className="footer__icons ">
+                    <a href="#">
+                        <img src="https://img.icons8.com/ios-filled/50/000000/github.png"/>
+                        <p>liampc@github.com</p>
+                    </a>
+                    <a href="#">
+                        <img src="https://img.icons8.com/fluent/48/000000/gmail--v2.png"/>
+                        <p>kathliampcarlos@gmail.com</p>
+                    </a>
+                    <a href="#">
+                        <img src="https://img.icons8.com/color/48/000000/linkedin.png"/>
+                        <p>www.linkedin.com/in/kathliampcarlos</p>
+                    </a>
+                    
                 </div>
             </div>
           
